1651e789370b8fb75aed7c7b00c77971d0d7f9c2
sarahbaranski <sbaranski@gmail.com>
2025-08-10 17:05:13 -0500

    feat: add asg

---
+++ asg.tf
@@ -0,0 +1,66 @@
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+# Autoscaling group
+name = var.application_asg_name
+
+min_size                  = 2
+max_size                  = 6
+desired_capacity          = 2
+wait_for_capacity_timeout = 0
+health_check_type         = "EC2"
+vpc_zone_identifier       = ["10.1.11.0/24", "10.1.12.0/24"]
+
+initial_lifecycle_hooks = [
+  {
+    name                  = "ApplicationStartupLifeCycleHook"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 60
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    # notification_metadata = jsonencode({ "hello" = "world" })
+  },
+  {
+    name                  = "ApplicationTerminationLifeCycleHook"
+    default_result        = "CONTINUE"
+    heartbeat_timeout     = 180
+    lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+    # notification_metadata = jsonencode({ "goodbye" = "world" })
+  }
+]
+
+instance_refresh = {
+  strategy = "Rolling"
+  preferences = {
+    checkpoint_delay       = 600
+    checkpoint_percentages = [35, 70, 100]
+    instance_warmup        = 300
+    min_healthy_percentage = 50
+    max_healthy_percentage = 100
+  }
+  triggers = ["tag"]
+}
+
+# Launch template
+launch_template_name        = "application-asg"
+launch_template_description = "Launch template application"
+update_default_version      = true
+
+image_id          = var.ami
+instance_type     = "t3.micro"
+ebs_optimized     = true
+enable_monitoring = true
+
+# This will ensure imdsv2 is enabled, required, and a single hop which is aws security
+# best practices
+# See https://docs.aws.amazon.com/securityhub/latest/userguide/autoscaling-controls.html#autoscaling-4
+metadata_options = {
+  http_endpoint               = "enabled"
+  http_tokens                 = "required"
+  http_put_response_hop_limit = 1
+}
+
+tags = {
+  Environment = "dev"
+  Project     = "coalfire"
+}
+}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,49 @@
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.1.0.0/16"
 }
 
 variable "private_subnet_ids" {
-  type = list(string)
+  type    = list(string)
   default = ["10.1.11.0/24", "10.1.12.0/24", "10.1.13.0/24", "10.1.14.0/24"]
 }
 
 variable "public_subnet_ids" {
-  type = list(string)
+  type    = list(string)
   default = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "vpc_sg_name" {
-  type = string
+  type    = string
   default = "vpc_sg"
+}
+
+variable "application_instance_name" {
+  type    = string
+  default = "application"
+}
+
+variable "management_instance_name" {
+  type    = string
+  default = "management"
+}
+
+variable "instance_size" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0de716d6197524dd9"
+}
+
+variable "key_name" {
+  type    = string
+  default = ""
+}
+
+variable "application_asg_name" {
+  type = string
+  default = "application-asg"
 }
