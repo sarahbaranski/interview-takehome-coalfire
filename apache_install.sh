38e6c65982fcbc69dffb92bf63011b21063110d8
sarahbaranski <sbaranski@gmail.com>
2025-08-15 09:49:35 -0500

    fix: correct the script to install apache

--- apache_install.sh
+++ apache_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#install apache
-sudo apt install -y httpd
+# Update package list
+sudo apt update
 
-#enable and start apache
-sudo systemctl enable httpd
-sudo systemctl start httpd
+# Install Apache (correct package name on Ubuntu)
+sudo apt install -y apache2
+
+# Enable and start Apache
+sudo systemctl enable apache2
+sudo systemctl start apache2
